55524ded07b8752b27e994aba05e9ed136cb3bad
LeonardM01 <martinisleo@gmail.com>
2022-09-23 21:56:57 +0200

    Added components and main screens

---
+++ components/Button.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const CircleButton = () => {
+  return (
+    <View>
+      <Text>CircleButton</Text>
+    </View>
+  )
+}
+
+export const RectButton = () => {
+  return (
+    <View>
+      <Text>RectButton</Text>
+    </View>
+  )
+}
---
+++ components/index.js
@@ -0,0 +1,6 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Button";
+
+export { HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton};
